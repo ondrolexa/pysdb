128e7749e78ef52f91af92f09c6f67a2af37b2a2
Ondrej Lexa <lexa.ondrej@gmail.com>
2019-08-28 08:59:54 +0200

    gpx fix 2

--- pysdb3/mainapp.py
+++ pysdb3/mainapp.py
@@ -236,7 +236,7 @@ class MainWindow(QtWidgets.QMainWindow):
             tree = etree.parse(str(fname))
             wpts = tree.findall("gpx:wpt", namespaces=NSMAP)
             sites = []
-            defunit_id = conn.execute("SELECT id FROM units LIMIT 1").fetchall()[0][0]
+            defunit_id = self.conn.execute("SELECT id FROM units LIMIT 1").fetchall()[0][0]
             for elem in wpts:
                 sites.append((elem.find('gpx:name', namespaces=NSMAP).text,   # name
                               float(elem.attrib['lon']),                      # x_coord
