f4b5ddca173552270446d1f5153a2c63ff2f2e1a
Ondrej Lexa <lexa.ondrej@gmail.com>
2025-02-07 09:48:14 +0100

    script updated

--- pysdb3.py
+++ pysdb3.py
@@ -1,15 +1,4 @@
-#!/usr/bin/env python3 
+from pysdb3.mainapp import main
 
-from pysdb3.mainapp import main as PysdbApp
-
-PysdbApp()
-
-# set tags
-        # for row in self.tagged:
-        #     item = QtGui.QListWidgetItem()
-        #     cb = QtGui.QCheckBox(row[1])
-        #     cb.setTristate(True)
-        #     cb.setCheckState(row[2])
-        #     item.setSizeHint(cb.sizeHint())
-        #     self.ui.taggedList.addItem(item)
-        #     self.ui.taggedList.setItemWidget(item, cb)
+if __name__ == "__main__":
+    main()
